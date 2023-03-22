eeea2fb49c83b0f5fabef43411b8c22b0b861f81
Matthew Clark <afromatt6288@hotmail.com>
2023-03-21 19:36:03 -0600

    Lab Complete

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,14 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
+
+# hello = Song("Hello", "25")
+# hello.save()
+# print(hello)
+
+# despacito = Song("Despacito", "Vida")
+# despacito.save()
+
+# songs = CURSOR.execute('SELECT * FROM songs')
+# [row for row in songs]
